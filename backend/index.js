d194d2b9a9ad5ed9c2e8176a79914859aeb43de3
sadhil <carsadhil@gmail.com>
2025-08-04 00:32:41 +0530

    finale

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        secure: true, // Use secure cookies in production
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 // 1 day
     }
